576b1348f18b89b1b0129f99876ce8ba81d2d668
최원석 <cws653@naver.com>
2020-12-21 19:51:15 +0900

    다시 문제풀어보기: 다이나믹프로그래밍
    가장 긴 증가하는 부분 수열
    포도주 시식

--- 3강 다이다믹 프로그래밍1/포도주 시식.py
+++ 3강 다이다믹 프로그래밍1/포도주 시식.py
@@ -1,20 +1,39 @@
+# 2차원 배열로 푸는 방
+# n = int(input())
+# s = [0 for i in range(n+1)]
+# d = [[0 for i in range(3)] for i in range(n+1)]
+#
+# for i in range(1, n+1):
+#     s[i] = int(input())
+#
+# # d[n][0] = max(d[n-1][0], d[n-1][1], d[n-1][2])
+# # d[n][1] = d[n-1][0] + s[n]
+# # d[n][2] = d[n-1][1] + s[n]
+# d[1][0] = s[0]
+# d[1][1] = s[1]
+# d[1][2] = s[0]
+# for i in range(2, n+1):
+#     d[i][0] = max(d[i-1][0], d[i-1][1], d[i-1][2])
+#     d[i][1] = d[i-1][0] + s[i]
+#     d[i][2] = d[i-1][1] + s[i]
+#
+# print(max(d[n]))
+
+# 1치원 배열로 푸는 방법
 n = int(input())
-s = []
-# d = [[0 for i in range(n)] for j in range(3)]
-d = [0 for i in range(n)]
+s = [0 for i in range(n+1)]
+d = [0 for i in range(n+1)]
 
-for i in range(n):
-    s.append(int(input()))
+for i in range(1, n+1):
+    s[i] = int(input())
 
-d[0] = s[0]
-d[1] = s[0] + s[1]
-
-for i in range(2, n):
-    # 0번 연속해서 마신 포도주
+d[1] = s[1]
+d[2] = s[1] + s[2]
+for i in range(3, n+1):
     d[i] = d[i-1]
     if d[i] < d[i-2] + s[i]:
         d[i] = d[i-2] + s[i]
-    if d[i] < d[i-3] + s[i] + s[i-1]:
-        d[i] = d[i-3] + s[i] + s[i-1]
+    if d[i] < d[i-3] + s[i-1] + s[i]:
+        d[i] = d[i-3] + s[i-1] + s[i]
 
-print(d[n-1])
+print(d[n])
