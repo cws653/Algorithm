c4c40686926e8714a78c67ae215c7b5126063587
최원석 <cws653@naver.com>
2020-12-23 18:47:56 +0900

    다시 문제풀어보기: 다이나믹프로그래밍
    가장 긴 바이토닉 부분 수열

--- 3강 다이다믹 프로그래밍1/가장 긴 바이토닉 부분 수열.py
+++ 3강 다이다믹 프로그래밍1/가장 긴 바이토닉 부분 수열.py
@@ -1,21 +1,23 @@
 n = int(input())
-a = list(map(int, input().split()))
-dpp = [0 for i in range(n)]
-dpm = [0 for i in range(n)]
-dpb = [0 for i in range(n)]
+s = list(map(int, input().split()))
+d1 = [0 for i in range(n)]
+d2 = [0 for i in range(n)]
 
 for i in range(n):
+    d1[i] = 1
     for j in range(i):
-        if a[i] > a[j] and dpp[i] < dpp[j]:
-            dpp[i] = dpp[j]
-    dpp[i] += 1
+        if s[j] < s[i] and d1[i] < d1[j] + 1:
+            d1[i] = d1[j] + 1
 
-for i in range(n-1, -1, -1):
-    for j in range(n-1, i, -1):
-        print(j)
-        if a[i] > a[j] and dpm[i] < dpm[j]:
-            dpm[i] = dpm[j]
-    dpm[i] += 1
+for i in range(n - 1, -1, -1):
+    d2[i] = 1
+    for j in range(n - 1, i - 1, -1):
+        if s[j] < s[i] and d2[i] < d2[j] + 1:
+            d2[i] = d2[j] + 1
 
+ans = 0
 for i in range(n):
-    dpb[i] = dpp[i] + dpm[i] - 1
+    if ans < d1[i] + d2[i] - 1:
+        ans = d1[i] + d2[i] - 1
+
+print(ans)
