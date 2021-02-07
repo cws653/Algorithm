8900fd1f42a63e8337f2e94847fbb99e8835ec7f
최원석 <cws653@naver.com>
2021-02-07 13:58:54 +0900

    다시 문제풀어보기: 그래프
    연결요소
    이분그래프

--- 백준/6강 그래프/연결요소.py
+++ 백준/6강 그래프/연결요소.py
@@ -1,18 +1,21 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10**6)
+
 n,m = map(int, sys.stdin.readline().split())
 s = [[0 for i in range(n+1)] for i in range(n+1)]
 visit = [0 for i in range(n+1)]
+
+def dfs(x):
+    visit[x] = 1
+    for y in range(1, n+1):
+        if s[x][y] == 1 and visit[y] == 0:
+            dfs(y)
+
+# 그래프 만들어주
 for i in range(m):
     a,b = map(int, sys.stdin.readline().split())
     s[a][b] = 1
     s[b][a] = 1
-
-def dfs(x):
-    visit[x] = 1
-    for i in range(1, n+1):
-        if visit[i] == 0 and s[x][i] == 1:
-            dfs(i)
 
 cnt = 0
 for i in range(1, n+1):
@@ -21,3 +24,5 @@ for i in range(1, n+1):
         cnt += 1
 
 print(cnt)
+
+
--- 백준/6강 그래프/이분그래프.py
+++ 백준/6강 그래프/이분그래프.py
@@ -1,12 +1,48 @@
+# import sys
+# from collections import deque
+#
+# t = int(sys.stdin.readline())
+#
+# def bfs(x):
+#     visit[x] = 1
+#     q = deque()
+#     q.append(x)
+#     while q:
+#         a = q.popleft()
+#         for b in range(1, v+1):
+#             if s[a][b] == 1 and visit[b] == 0:
+#                 visit[b] = -visit[a]
+#                 q.append(b)
+#             else:
+#                 if s[a][b] == 1 and visit[a] == visit[b]:
+#                     return False
+#     return True
+#
+# for _ in range(t):
+#     v,e = map(int, sys.stdin.readline().split())
+#     s = [[0 for _ in range(v+1)] for _ in range(v+1)]
+#     visit = [0 for _ in range(v+1)]
+#     isTrue = True
+#     for _ in range(e):
+#         a,b = map(int, sys.stdin.readline().split())
+#         s[a][b] = 1
+#         s[b][a] = 1
+#     for i in range(1, v+1):
+#         if visit[i] == 0:
+#             if not bfs(i):
+#                 isTrue = False
+#                 break
+#
+#     print("Yes" if isTrue else "No")
+
 import sys
 from collections import deque
+t = int(sys.stdin.readline())
 
-k = int(sys.stdin.readline())
-
-def bfs(start):
-    visit[start] = 1
+def bfs(x):
+    visit[x] = 1
     q = deque()
-    q.append(start)
+    q.append(x)
     while q:
         a = q.popleft()
         for i in s[a]:
@@ -18,18 +54,19 @@ def bfs(start):
                     return False
     return True
 
-for i in range(k):
+
+for _ in range(t):
     v,e = map(int, sys.stdin.readline().split())
+    visit = [0 for i in range(v + 1)]
     s = [[] for i in range(v+1)]
-    visit = [0 for i in range(v+1)]
     isTrue = True
-    for j in range(e):
+    for _ in range(e):
         a,b = map(int, sys.stdin.readline().split())
         s[a].append(b)
         s[b].append(a)
-    for k in range(1, v+1):
-        if visit[k] == 0:
-            if not bfs(k):
+    for i in range(1, v+1):
+        if visit[i] == 0:
+            if not bfs(i):
                 isTrue = False
-                break
-    print("YES"if isTrue else "NO")
+
+    print("YES" if isTrue else "NO")
