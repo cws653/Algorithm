b1d9873ffeb55a4dc8d8aa0a09fd053b00f77ab9
최원석 <cws653@naver.com>
2021-02-13 18:52:22 +0900

    다시 문제풀어보기: 그래프
    미로
    토마토

--- 백준/6강 그래프/미로.py
+++ 백준/6강 그래프/미로.py
@@ -1,24 +1,52 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
+from collections import deque
+
+n,m = map(int, sys.stdin.readline().split())
 s = []
-for i in range(n):
-    s.append(list(sys.stdin.readline()))
+for _ in range(n):
+    s.append(list(map(int, sys.stdin.readline().strip())))
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+# 상하좌우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-def bfs(i,j):
-    s[i][j] = 1
-    queue = [[i,j]]
-    while queue:
-        a,b = queue[0][0], queue[0][1]
-        del queue[0]
-        for k in range(4):
-            x = a + dx[k]
-            y = b + dy[k]
-            if 0 <= x < n and 0 <= y < m and s[x][y] == "1":
-                queue.append([x,y])
-                s[x][y] = s[a][b] + 1
+def bfs(x,y):
+    s[x][y] = 1
+    q = deque()
+    q.append([x,y])
+    while q:
+        a,b = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and s[nx][ny] == 1:
+                q.append([nx,ny])
+                s[nx][ny] = s[a][b] + 1
 
 bfs(0,0)
 print(s[n-1][m-1])
+
+# import sys
+# n, m = map(int, sys.stdin.readline().split())
+# s = []
+# for i in range(n):
+#     s.append(list(sys.stdin.readline()))
+#
+# dx = [1,-1,0,0]
+# dy = [0,0,1,-1]
+#
+# def bfs(i,j):
+#     s[i][j] = 1
+#     queue = [[i,j]]
+#     while queue:
+#         a,b = queue[0][0], queue[0][1]
+#         del queue[0]
+#         for k in range(4):
+#             x = a + dx[k]
+#             y = b + dy[k]
+#             if 0 <= x < n and 0 <= y < m and s[x][y] == "1":
+#                 queue.append([x,y])
+#                 s[x][y] = s[a][b] + 1
+#
+# bfs(0,0)
+# print(s[n-1][m-1])
--- 백준/6강 그래프/토마토.py
+++ 백준/6강 그래프/토마토.py
@@ -1,39 +1,41 @@
 import sys
 from collections import deque
 
-def bfs(x,y,s):
-    days = -1
-
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
-
-    while mydeque:
-        days += 1
-        for _ in range(len(mydeque)):
-            a,b = mydeque.popleft()
-
-            for i in range(4):
-                nx = a + dx[i]
-                ny = b + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and s[nx][ny] == 0:
-                    s[nx][ny] = s[a][b] + 1
-                    mydeque.append([nx,ny])
-
-    for i in s:
-        if 0 in i:
-          return -1
-    return days
-
 m,n = map(int, sys.stdin.readline().split())
 s = []
-mydeque = deque()
-for i in range(n):
+queue = deque()
+
+# 상하좌우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+for _ in range(n):
     s.append(list(map(int, sys.stdin.readline().split())))
 
+def bfs():
+    while queue:
+        a,b = queue.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and s[nx][ny] == 0:
+                s[nx][ny] = s[a][b] + 1
+                queue.append([nx,ny])
 for i in range(n):
     for j in range(m):
         if s[i][j] == 1:
-            mydeque.append([i,j])
-
-print(bfs(m,n,s))
-
+            queue.append([i,j])
+bfs()
+isTrue = False
+result = -2
+for i in s:
+    for j in i:
+        if j == 0:
+            isTrue = True
+        result = max(result, j)
+if isTrue == True:
+    print(-1)
+elif result == -1:
+    print(0)
+else:
+    print(result - 1)
