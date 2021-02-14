d222decad5695e0839409fef2d25e5bc5b3230d4
최원석 <cws653@naver.com>
2021-02-15 01:32:59 +0900

    다시 문제풀어보기: 그래프
    다리 만들기

--- 백준/6강 그래프/다리 만들기.py
+++ 백준/6강 그래프/다리 만들기.py
@@ -1,61 +1,58 @@
 import sys
 from collections import deque
 
-sys.setrecursionlimit(10 ** 6)
+n = int(sys.stdin.readline())
+s = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+visit = [[0]*n for _ in range(n)]
+q = deque()
+cnt = 1
 
 # 상하좌우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-def dfs(x,y):
-    global cnt
-    visit[x][y] = 1
-    graph[x][y] = cnt
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n:
-            if visit[nx][ny] == 0 and graph[nx][ny]:
-                dfs(nx, ny)
-
-def bfs(z):
-    global ans
-    dist = [[-1]*n for _ in range(n)]
-    queue = deque()
-
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == z:
-                queue.append([i,j])
-                dist[i][j] = 0
-
-    while queue:
-        x, y = queue.popleft()
+def bfs1(x,y,cnt):
+    visit[x][y] = cnt
+    q.append([x,y])
+    while q:
+        a,b = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = a + dx[i]
+            ny = b + dy[i]
             if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] > 0 and graph[nx][ny] != z:
-                    ans = min(ans, dist[x][y])
-                    return
-                if graph[nx][ny] == 0 and dist[nx][ny] == -1:
-                    dist[nx][ny] = dist[x][y] + 1
-                    queue.append([nx, ny])
+                if s[nx][ny] == 1 and visit[nx][ny] == 0:
+                    visit[nx][ny] = cnt
+                    q.append([nx, ny])
 
-input = sys.stdin.readline
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-visit = [[0]*n for _ in range(n)]
-cnt = 1
-ans = sys.maxsize
+def bfs2(num):
+    while q2:
+        a,b = q2.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if visit[nx][ny] != num and s[nx][ny] == 1:
+                    return visit2[a][b] - 1
+                if visit2[nx][ny] == 0 and s[nx][ny] == 0:
+                    q2.append([nx,ny])
+                    visit2[nx][ny] += visit2[a][b] + 1
+
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1 and visit[i][j] == 0:
-            dfs(i,j)
+        if s[i][j] == 1 and visit[i][j] == 0:
+            bfs1(i,j,cnt)
             cnt += 1
 
-for i in range(1, cnt):
-    bfs(i)
+ans = sys.maxsize
+for k in range(1, cnt):
+    q2 = deque()
+    visit2 = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if s[i][j] == 1 and visit[i][j] == k:
+                q2.append([i, j])
+                visit2[i][j] = 1
+    result = bfs2(k)
+    ans = min(ans, result)
 
 print(ans)
