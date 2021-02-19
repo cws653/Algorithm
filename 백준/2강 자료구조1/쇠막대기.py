a39c93487bd44bb104eedc6b259bf037e8ac3604
최원석 <cws653@naver.com>
2021-02-19 21:37:49 +0900

    다시 문제풀어보기2: 자료구조
    쇠막대기
    에디터

--- 백준/2강 자료구조1/쇠막대기.py
+++ 백준/2강 자료구조1/쇠막대기.py
@@ -1,17 +1,16 @@
+bar_razor = list(input())
+stack = []
+answer = 0
 
-a = list(input())
-s = []
-b = 0
-
-for i in range(len(a)):
-    if a[i] == '(':
-        s.append(a[i])
+for i in range(len(bar_razor)):
+    if bar_razor[i] == '(':
+        stack.append('(')
     else:
-        if a[i-1] == '(':
-            s.pop()
-            b = b + len(s)
+        if bar_razor[i-1] == '(':
+            stack.pop()
+            answer += len(stack)
         else:
-            s.pop()
-            b = b + 1
+            stack.pop()
+            answer += 1
 
-print(b)
+print(answer)
--- 백준/2강 자료구조1/에디터.py
+++ 백준/2강 자료구조1/에디터.py
@@ -1,63 +1,29 @@
-
-
-# # 소스 1번
-# inputString = input().strip()
-# inputInt = int(input())
-# position = len(inputString) + 1
-# print(position)
-#
-# for i in range(inputInt):
-#     inputDetail = input()
-#     # print(inputDetail)
-#     if "P" in inputDetail:
-#         # print(inputString[0:position - 1])
-#         # print(inputString[position - 1:])
-#         inputString = inputString[0:position - 1] + inputDetail[2] + inputString[position - 1:]
-#         position += 1
-#     elif "L" in inputDetail:
-#         if position == 1:
-#             position = position
-#         else:
-#             position -= 1
-#     elif "D" in inputDetail:
-#         if position == position + 1:
-#             position = position
-#         else:
-#             position += 1
-#     elif "B" in inputDetail:
-#         if position == 1:
-#             position == position
-#         else:
-#             inputString = inputString[0:position - 2] + inputString[position - 1:]
-#             position -= 1
-#     print(position)
-#     print(inputString)print
-#
-#
-#
-# # 소스 2번
-
 import sys
-leftStack = list(sys.stdin.readline()[:-1])
-# print(leftStack)
-rightStack = list()
-# print(rightStack)
-testCase = int(sys.stdin.readline())
-# print(testCase)
+a = list(sys.stdin.readline().rstrip())
+n = int(sys.stdin.readline())
+stack_right = []
+stack_left = []
+stack = []
 
-for _ in range(testCase):
-    cmd = sys.stdin.readline()
-    if cmd[0] == 'L' and leftStack:
-        rightStack.append(leftStack.pop())
-    elif cmd[0] == 'D' and rightStack:
-        leftStack.append(rightStack.pop())
-    elif cmd[0] == 'B' and leftStack:
-        leftStack.pop()
-    elif cmd[0] == 'P':
-        leftStack.append(cmd[2])
+for i in range(len(a)):
+    stack_left.append(a[i])
 
-# print(rightStack)
-# print(rightStack[:-1])
-# print(rightStack[::])
-# print(rightStack[::-1])
-sys.stdout.write(''.join(leftStack + rightStack[::-1]))
+for i in range(n):
+    b = list(sys.stdin.readline().split())
+    if b[0] == 'L':
+        if stack_left:
+            x = stack_left.pop()
+            stack_right.append(x)
+    elif b[0] == 'D':
+        if stack_right:
+            x = stack_right.pop()
+            stack_left.append(x)
+    elif b[0] == 'B':
+        if stack_left:
+            stack_left.pop()
+    elif b[0] == 'P':
+        stack_left.append(b[1])
+
+stack = stack_left + list(reversed(stack_right))
+str_stack = ''.join(stack)
+print(str_stack)
