62efa01c3f0c5cdc8efaeaada75c5c757ea34b0c
최원석 <cws653@naver.com>
2021-02-24 21:28:36 +0900

    다시 문제풀어보기2: 자료구조
    네 수
    접미사 배열

--- 백준/2강 자료구조1/네 수.py
+++ 백준/2강 자료구조1/네 수.py
@@ -1,12 +1,8 @@
-# input = input().split()
-#
-# a = input[0] + input[1]
-# b = input[2] + input[3]
-#
-# inta = int(a)
-# intb = int(b)
-#
-# sum = inta + intb
-#
-# print(sum)
-
+import sys
+a,b,c,d = map(int, sys.stdin.readline().split())
+x = str(a)+str(b)
+y = str(c)+str(d)
+int_x = int(x)
+int_y = int(y)
+answear = int_x + int_y
+print(answear)
--- 백준/2강 자료구조1/접미사 배열.py
+++ 백준/2강 자료구조1/접미사 배열.py
@@ -1,13 +1,22 @@
-input = input()
-input = list(input)
-myStrings = []
+# import sys
+# s = list(sys.stdin.readline().rstrip())
+# stack = []
+# stack.append(''.join(s))
+# for i in range(len(s)-1):
+#     del s[0]
+#     stack.append(''.join(s))
+#
+# stack.sort()
+#
+# for a in stack:
+#     print(a)
 
-for i in range(len(input)):
-    myStrings.append(''.join(input))
-    input.pop(0)
+S = str(input())
+S_list = []
 
-result = sorted(myStrings)
+for _ in S:
+    S_list.append(S)
+    S = S[1:]
 
-for word in result:
-    print(word)
-
+for i in sorted(S_list):
+    print(i)
