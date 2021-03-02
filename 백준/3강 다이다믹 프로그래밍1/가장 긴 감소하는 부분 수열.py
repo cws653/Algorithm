19bb1c0e68d7768735473787a905c507b2883669
최원석 <cws653@naver.com>
2021-03-03 01:01:33 +0900

    다시 문제풀어보기2: 다이나믹프로그래밍
    가장 긴 감소하는 부분 수열
    가장 긴 바이토닉 부분 수열

--- 백준/3강 다이다믹 프로그래밍1/가장 긴 감소하는 부분 수열.py
+++ 백준/3강 다이다믹 프로그래밍1/가장 긴 감소하는 부분 수열.py
@@ -1,11 +1,23 @@
+# n = int(input())
+# s = list(map(int, input().split()))
+# dp = [0 for i in range(n)]
+#
+# for i in range(n):
+#     dp[i] = 1
+#     for j in range(i):
+#         if s[i] < s[j] and dp[i] < dp[j] + 1:
+#             dp[i] = dp[j] + 1
+#
+# print(max(dp))
+
 n = int(input())
 s = list(map(int, input().split()))
-d = [0 for i in range(n)]
+dp = [0 for i in range(n)]
 
-for i in range(n):
-    d[i] = 1
-    for j in range(i):
-        if s[i] < s[j] and d[i] < d[j] + 1:
-            d[i] = d[j] + 1
+for i in range(n - 1, -1, -1):
+    dp[i] = 1
+    for j in range(n - 1, i, -1):
+        if s[j] < s[i] and dp[i] < dp[j] + 1:
+            dp[i] = dp[j] + 1
 
-print(max(d))
+print(max(dp))
--- 백준/3강 다이다믹 프로그래밍1/가장 긴 바이토닉 부분 수열.py
+++ 백준/3강 다이다믹 프로그래밍1/가장 긴 바이토닉 부분 수열.py
@@ -1,3 +1,23 @@
+# n = int(input())
+# s = list(map(int, input().split()))
+# dp1 = [0 for i in range(n)]
+# dp2 = [0 for i in range(n)]
+#
+# for i in range(n):
+#     dp1[i] = 1
+#     for j in range(i):
+#         if s[i] > s[j] and dp1[i] < dp1[j] + 1:
+#             dp1[i] = dp1[j] + 1
+#
+# a = dp1.index(max(dp1))
+# for i in range(a, n):
+#     dp2[i] = 1
+#     for j in range(i):
+#         if s[j] > s[i] and dp2[i] < dp2[j] + 1:
+#             dp2[i] = dp2[j] + 1
+#
+# print(max(dp1) + max(dp2) - 1)
+
 n = int(input())
 s = list(map(int, input().split()))
 d1 = [0 for i in range(n)]
