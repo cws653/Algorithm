cbaea791358b527a98cba21f95aa959d64b7c6af
최원석 <cws653@naver.com>
2021-03-04 23:59:49 +0900

    다시 문제풀어보기2: 다이나믹프로그래밍
    제곱수의 합
    타일 채우기

--- 백준/3강 다이다믹 프로그래밍1/제곱수의 합.py
+++ 백준/3강 다이다믹 프로그래밍1/제곱수의 합.py
@@ -1,27 +1,11 @@
-# n = int(input())
-# d = [0 for i in range(n+1)]
-# square = [i*i for i in range(1, 317)]
-#
-# # d[i] = min(d[i-j^2] + 1)
-# for i in range(1, n+1):
-#     d[i] = i
-#     for j in square:
-#         if j > i:
-#             break
-#         else:
-#             d[i] > d[i - j] + 1
-#             d[i] = d[i - j] + 1
-#
-# print(d[n])
-
 n = int(input())
-d = [0 for i in range(n+1)]
-square = [i * i for i in range(1, 317)]
-for i in range(1, n+1):
+dp = [0 for i in range(n+1)]
+square = [i*i for i in range(1,317)]
+for i in range(1,n+1):
     s = []
     for j in square:
         if j > i:
             break
-        s.append(d[i-j])
-    d[i] = min(s) + 1
-print(d[n])
+        s.append(dp[i-j])
+    dp[i] = min(s) + 1
+print(dp[n])
--- 백준/3강 다이다믹 프로그래밍1/타일 채우기.py
+++ 백준/3강 다이다믹 프로그래밍1/타일 채우기.py
@@ -1,10 +1,9 @@
 n = int(input())
-d = [0 for i in range(n+1)]
-d[0] = 1
-
-for i in range(2, n+1, 2):
-    d[i] = 3 * d[i-2]
+dp = [0 for i in range(n+1)]
+dp[0] = 1
+for i in range(2,n+1,2):
+    dp[i] = 3*dp[i-2]
     for j in range(i-4, -1, -2):
-        d[i] += 2 * d[j]
+        dp[i] += dp[j]*2
 
-print(d[n])
+print(dp[n])
